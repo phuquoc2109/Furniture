fd8c50dd2f6dabda552be23e5439a11b1d8314ca
phuquoc <quocduongphu@gmail.com>
2021-04-02 16:15:44 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {  
+function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
